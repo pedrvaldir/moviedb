de29d3d16bb82d160fd735c12493d1f3cf15c29f
Valdir <pedrvaldir@gmail.com>
2019-02-26 02:35:15 -0300

    primeiro commit

---
+++ app/src/main/java/moviedbcom/valdir/themoviedb/Movie.java
@@ -0,0 +1,11 @@
+package moviedbcom.valdir.themoviedb;
+
+public class Movie {
+    public String title;
+    public String image;
+    public String overview;
+    public String vote_overage;
+    public String date_release;
+
+
+}
---
+++ app/src/main/java/moviedbcom/valdir/themoviedb/utils/Constants.java
@@ -0,0 +1,9 @@
+package moviedbcom.valdir.themoviedb.utils;
+
+public class Constants {
+    public String keyMdb = "3addae156b95f76aff58749c8daf2d57";
+    public String urlMdb = "https://api.themoviedb.org/3/";
+    public String urlPopularity = "/discover/movie?sort_by=popularity.desc";
+    public String urlVoteOverage = "/discover/movie?sort_by=popularity.desc";
+
+}
